9dbb604f090d9b299d60ac4be870e4eda46f3b2b
Syed Saleem Raza Shahit <syedsaleemrazashah69@gmail.com>
2024-03-17 18:24:10 +0500

    Practice

---
+++ app.js
@@ -0,0 +1,15 @@
+variables pratices
+
+let Name = ("Syed Saleem Raza Shah")
+console.log(Name)
+
+let age = "50";
+
+console.log(50 + 4)
+console.log("50 + 4")
+console.log("50" + 4)
+console.log(age + 4)
+console.log("age + 4")
+console.log("age" + 4)
+
+
