1fe1e329beec40f0fdcf525029ea24d6f70fca37
Syed Saleem Raza Shahh <syedsaleemrazashah69@gmail.com>
2024-04-20 18:07:03 +0500

    practice

---
+++ variebles/app.js
@@ -0,0 +1,13 @@
+// Variebles
+
+let name = "saleem";
+console.log(name);
+
+let age = "12";
+
+console.log(age + 4);
+console.log(12 + 4);
+console.log("12 + 4");
+console.log("age + 4");
+console.timeLog(age + "4");
+console.log("12" + 4);
