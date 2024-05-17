a682fed3363dded991f47cecf0e5843bdf2dde03
Syed Saleem Raza Shahh <syedsaleemrazashah69@gmail.com>
2024-05-17 14:13:58 +0500

    object

--- variebles/app.js
+++ variebles/app.js
@@ -1,13 +1,31 @@
 // Variebles
 
-let name = "saleem";
-console.log(name);
+// let name = "saleem";
+// console.log(name);
 
-let age = "12";
+// let age = "12";
 
-console.log(age + 4);
-console.log(12 + 4);
-console.log("12 + 4");
-console.log("age + 4");
-console.timeLog(age + "4");
-console.log("12" + 4);
+// console.log(age + 4);
+// console.log(12 + 4);
+// console.log("12 + 4");
+// console.log("age + 4");
+// console.timeLog(age + "4");
+// console.log("12" + 4);
+
+// object
+
+// let teacher = [saleem, email, 546, WMA - 173503];
+
+let myName = "saleem";
+
+let astudent = {
+  key: "value",
+  name: myName,
+  age: 19,
+  roll: "WMA - 173503",
+  ispresent: true,
+  hobbies: ["cricket", "football", "web-developer"],
+  teacher: { name: "saleem", age: 19 },
+  print: {},
+};
+console.log(teacher);
