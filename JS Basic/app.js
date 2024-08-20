fd8ea2b5ce521d148d6870fb8492e2aa4e58f787
Syed Saleem Raza Shah <syedsaleemrazashah69@gmail.com>
2024-08-20 13:05:21 +0500

    Js-basic

---
+++ JS Basic/app.js
@@ -0,0 +1,16 @@
+// let mein variable ka reference change ho jata hai
+// const mein variable ka reference change nahi hota hai
+
+// array aur object hamesha const se bane ga 90%
+// const 95% used hota hai javascript mein
+
+// // js mein 7 primitive  Data types hoti hein
+
+// Boolean
+// Number
+// Symbol
+// BigInt
+// Object
+// undefined
+// null
+// String
---
+++ variable/app.js
@@ -0,0 +1,10 @@
+let a = 6;
+a = 4;
+console.log(a);
+
+var b = 2;
+b = 6;
+console.log(b);
+
+const c = a2;
+console.log(c);
